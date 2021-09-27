1cc515684aed5121e8f038c6d96dfb8662eeb58f
CindyKatoni <katmuema@gmail.com>
2021-09-27 13:26:34 +0300

    Create components folder in views and add the master layout file and use slot to add other blade views. Episode 61

---
+++ resources/views/components/app.blade.php
@@ -0,0 +1,26 @@
+<x-master>
+
+<section class="px-8">
+    <main class="container mx-auto">
+        <div class="lg:flex lg:justify-center">
+            <!--Sidebar section-->
+            <div class="lg:w-32">
+                @include ('_sidebar-links')
+            </div>
+
+
+            <!--Publish section-->
+            <div class="lg:flex-1 lg:mx-10" style="max-width: 700px">
+                {{ $slot }}
+            </div>
+
+
+            <!--Friends section-->
+            <div class="lg:w-1/6">
+                @include ('_friends-list')
+            </div>
+        </div>
+    </main>
+</section>
+        
+</x-master>
