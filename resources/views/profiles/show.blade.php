fc9d300b7042cf7ee0fd065f65747d7f5dc235d4
CindyKatoni <katmuema@gmail.com>
2021-09-27 17:00:41 +0300

    Use if conditional to allow owner of the profile to edit. And display follow or unfollow button if the auth user is not the user's profile. Episode 63

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -10,14 +10,18 @@
         </div>
 
         <div class="flex">
+            @if (auth()->user()->is($user))
             <a href="" class="rounded-full border border-gray-300 py-2 px-4 text-black text-sm">Edit Profile</a>
+            @endif
             
+            @if (auth()->user()->isNot($user))
             <form method="POST" action="/profiles/{{ $user->name }}/follow">
             @csrf
                 <button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-sm">
                     {{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me'}}
                 </button>
             </form>
+            @endif
            
         </div>
         </div>
