df6088286cb4465ffe85d3ab986819b719475f22
CindyKatoni <katmuema@gmail.com>
2021-09-28 16:23:14 +0300

    Add Cancel button form in profile edit.  Ep 66

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -84,13 +84,18 @@
                 <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
             @enderror
         </div>
-
+        
         <div class="mb-6">
-            <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mr-4">
+            <button type="submit"
+                    class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mr-4"
+            >
                 Submit
             </button>
 
+            <a href="{{ $user->path() }}" class="hover:underline">Cancel</a>
         </div>
+
+        
 
     </form>
 
