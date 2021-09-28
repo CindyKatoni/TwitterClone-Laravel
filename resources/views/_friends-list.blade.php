0cc8dd158fa4833116d30823572ad28f48ca404f
CindyKatoni <katmuema@gmail.com>
2021-09-28 14:03:42 +0300

    Editing spaces from code

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,9 +1,9 @@
 <h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @forelse (auth()->user()->follows as $user)
+    @forelse (current_user()->follows as $user)
     <li class="mb-4">
         <div >
-            <a href="{{ route('profile', $user) }}" class="flex items-center text-sm">
+            <a href="{{ $user->path() }}" class="flex items-center text-sm">
             <img src="{{ $user->avatar }}" class="rounded-full mr-2" width="50" height="50" alt="">
             {{ $user->name }}
             </a>
--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,12 +1,12 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
                 <form method="POST" action="/tweets">
                 @csrf
-                    <textarea name="body" class="w-full" placeholder="What's up doc?"></textarea>
+                    <textarea name="body" class="w-full" placeholder="What's up?"></textarea>
                     <hr class="my-4">
                     <footer class="flex justify-between">
                    
-                    <img src="{{ auth()->user()->avatar }}" class="rounded-full mr-2" alt="">
-                    <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-A-Roo</button>
+                    <img src="{{ auth()->user()->avatar }}" class="rounded-full mr-2" alt="your avatar" height="50" width="50" >
+                    <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Submit Tweet</button>
                     </footer>
                 </form>
 
