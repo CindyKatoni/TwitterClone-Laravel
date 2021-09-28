77806793c8b5bbe9e0216b9b1cc26a7f59e3718c
CindyKatoni <katmuema@gmail.com>
2021-09-28 16:27:32 +0300

    edited Ep 66

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,7 +1,8 @@
+<div class="bg-gray-200 border border-gray-300 rounded-lg py-4 px-6">
 <h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
     @forelse (current_user()->follows as $user)
-    <li class="mb-4">
+    <li class="{{ $loop->last ? '' : 'mb-4' }}">
         <div >
             <a href="{{ $user->path() }}" class="flex items-center text-sm">
             <img src="{{ $user->avatar }}" class="rounded-full mr-2" width="50" height="50" alt="">
@@ -13,3 +14,4 @@
     <p class="p-4"> No Friends Yet...</p>
     @endforelse
 </ul>
+</div>
