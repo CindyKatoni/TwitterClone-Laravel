002f21bf63b3bf51ad9ade6db9b7803aed997010
CindyKatoni <katmuema@gmail.com>
2021-09-28 16:27:57 +0300

    Paginate

--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,12 +1,12 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
                 <form method="POST" action="/tweets">
                 @csrf
-                    <textarea name="body" class="w-full" placeholder="What's up?"></textarea>
+                    <textarea name="body" class="w-full" placeholder="What's up?" required autofocus></textarea>
                     <hr class="my-4">
-                    <footer class="flex justify-between">
+                    <footer class="flex justify-between items-center">
                    
                     <img src="{{ auth()->user()->avatar }}" class="rounded-full mr-2" alt="your avatar" height="50" width="50" >
-                    <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Submit Tweet</button>
+                    <button type="submit" class="bg-blue-500 hover:bg-blue-600 rounded-lg shadow px-10 text-sm text-white h-10">Submit Tweet</button>
                     </footer>
                 </form>
 
--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -11,7 +11,7 @@
 
         @endforeach
 
-        {{ $users->links() }}
-
+        {{ $users->links('pagination::tailwind') }}
+        
     </div>
 </x-app>
